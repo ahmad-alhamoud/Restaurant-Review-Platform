85d5f737d25ed8720300d991f0c2be963104756c
Ahmad Alhamoud <alhamoudahmad2000@gmail.com>
2025-09-03 21:52:36 +0300

    created the domain entities for the platform.

---
+++ src/main/java/com/ahmad/restaurant/repositories/RestaurantRepository.java
@@ -0,0 +1,11 @@
+package com.ahmad.restaurant.repositories;
+
+import com.ahmad.restaurant.domain.entities.Restaurant;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantRepository extends ElasticsearchRepository<Restaurant, String>{
+
+
+}
