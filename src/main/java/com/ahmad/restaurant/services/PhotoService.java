64bea01bb4cbaf97f563e6da49806c4b5ea814f1
Ahmad Alhamoud <alhamoudahmad2000@gmail.com>
2025-09-18 18:17:04 +0300

    Implement file system storage service.

---
+++ src/main/java/com/ahmad/restaurant/services/PhotoService.java
@@ -0,0 +1,12 @@
+package com.ahmad.restaurant.services;
+
+import com.ahmad.restaurant.domain.entities.Photo;
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Optional;
+
+public interface PhotoService {
+    Photo uploadPhoto(MultipartFile file);
+    Optional<Resource> getPhotoAsResource(String id);
+}
