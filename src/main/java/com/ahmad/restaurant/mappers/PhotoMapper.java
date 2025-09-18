1c134c5e688e02a02efe2eb71c6bbe9e068c3029
Ahmad Alhamoud <alhamoudahmad2000@gmail.com>
2025-09-18 18:32:28 +0300

    Photo Dto & Mapper.

---
+++ src/main/java/com/ahmad/restaurant/mappers/PhotoMapper.java
@@ -0,0 +1,11 @@
+package com.ahmad.restaurant.mappers;
+
+import com.ahmad.restaurant.domain.dtos.PhotoDto;
+import com.ahmad.restaurant.domain.entities.Photo;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface PhotoMapper {
+    PhotoDto toDto(Photo photo);
+}
