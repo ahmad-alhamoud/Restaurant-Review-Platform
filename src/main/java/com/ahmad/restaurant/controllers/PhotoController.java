a1c4c24710110b99269a69d76b53f14ba57bb734
Ahmad Alhamoud <alhamoudahmad2000@gmail.com>
2025-09-20 00:18:45 +0300

    Photo upload endpoint.

---
+++ src/main/java/com/ahmad/restaurant/controllers/PhotoController.java
@@ -0,0 +1,28 @@
+package com.ahmad.restaurant.controllers;
+
+import com.ahmad.restaurant.domain.dtos.PhotoDto;
+import com.ahmad.restaurant.domain.entities.Photo;
+import com.ahmad.restaurant.mappers.PhotoMapper;
+import com.ahmad.restaurant.services.PhotoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/api/photos")
+public class PhotoController {
+
+    private final PhotoService photoService;
+    private final PhotoMapper photoMapper;
+
+    @PostMapping
+    public PhotoDto uploadPhoto(@RequestParam("file") MultipartFile file) {
+        Photo savedPhoto = photoService.uploadPhoto(file);
+        return photoMapper.toDto(savedPhoto);
+    }
+
+}
