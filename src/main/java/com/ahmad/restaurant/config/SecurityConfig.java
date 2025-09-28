1a1b13265c0520c8a172f12aa053ba1388f2234b
Ahmad Alhamoud <alhamoudahmad2000@gmail.com>
2025-09-28 17:54:44 +0300

    Photo retrieve endpoint.

--- src/main/java/com/ahmad/restaurant/config/SecurityConfig.java
+++ src/main/java/com/ahmad/restaurant/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.ahmad.restaurant.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -14,8 +15,10 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(
-                        auth -> auth.anyRequest().authenticated()
+        http.authorizeHttpRequests(auth ->
+                        auth
+                                .requestMatchers(HttpMethod.GET, "/api/photos/**").permitAll()
+                                .anyRequest().authenticated()
                 )
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
