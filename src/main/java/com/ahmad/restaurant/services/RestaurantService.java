af72607d8e802d4487a3b83052c73fc680b2a798
Ahmad Alhamoud <alhamoudahmad2000@gmail.com>
2025-10-23 15:38:17 +0300

    Implement search restaurants method.

--- src/main/java/com/ahmad/restaurant/services/RestaurantService.java
+++ src/main/java/com/ahmad/restaurant/services/RestaurantService.java
@@ -2,7 +2,18 @@ package com.ahmad.restaurant.services;
 
 import com.ahmad.restaurant.domain.RestaurantCreateUpdateRequest;
 import com.ahmad.restaurant.domain.entities.Restaurant;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface RestaurantService {
     Restaurant createRestaurant(RestaurantCreateUpdateRequest request);
+
+    Page<Restaurant> searchRestaurants(
+            String query,
+            Float minRating,
+            Float latitude,
+            Float longitude,
+            Float radius,
+            Pageable pageable
+    );
 }
